d1ddf1ac7d059a819c1c6c9c2e36d2b907197c54
LuciaFores <77293908+LuciaFores@users.noreply.github.com>
2023-07-09 17:27:29 +0200

    Update get_reviews.py
    - Changed file opening

--- Project/dataset_collection/get_reviews.py
+++ Project/dataset_collection/get_reviews.py
@@ -43,38 +43,38 @@ else:
     resolved_steam_ids = df_resolved['steam_id'].tolist()
     steam_ids = list(set(steam_ids) - set(resolved_steam_ids))
 
-with open('output_files/game_reviews.csv', 'a') as F:
-    with open('output_files/error_steam_ids.csv', 'a') as F_1:
-        with open('output_files/resolved_steam_ids.csv', 'a') as F_2:
-            writer = csv.writer(F)
-            writer_1 = csv.writer(F_1)
-            writer_2 = csv.writer(F_2)
-            for steam_id in tqdm(steam_ids, total=len(steam_ids)):
-                i = 0
-                time.sleep(1.5)
-                while True:
-                    i += 1
-                    try:                   
-                        reviews = get_reviews(steam_id, i)
-                    except:
-                        writer_1.writerow([steam_id])
-                        break
-                    if reviews != []:
-                        for review in reviews:
-                            review_data = []
-                            review_data.append(str(steam_id))
-                            app_id = review.find('a', {'class': 'game_capsule_ctn'})['href'][31:]
-                            review_data.append(app_id)
-                            recommended = review.find('div', {'class': 'title'}).text
-                            review_data.append(recommended)
-                            hours_played = review.find('div', {'class': 'hours'}).text
-                            hours_played = hours_played.replace('\r', '')
-                            hours_played = hours_played.replace('\n', '')
-                            hours_played = hours_played.replace('\t', '')
-                            review_data.append(hours_played)
-                            writer.writerow(review_data)                        
-                    else:
-                        writer_2.writerow([steam_id])
-                        break
+for steam_id in tqdm(steam_ids, total=len(steam_ids)):
+    i = 0
+    time.sleep(1.5)
+    while True:
+        i += 1
+        try:                   
+            reviews = get_reviews(steam_id, i)
+        except:
+            with open('output_files/error_steam_ids.csv', 'a') as F_1:
+                writer_1 = csv.writer(F_1)
+                writer_1.writerow([steam_id])
+                break
+        if reviews != []:
+            with open('output_files/game_reviews.csv', 'a') as F:
+                writer = csv.writer(F)
+                for review in reviews:
+                    review_data = []
+                    review_data.append(str(steam_id))
+                    app_id = review.find('a', {'class': 'game_capsule_ctn'})['href'][31:]
+                    review_data.append(app_id)
+                    recommended = review.find('div', {'class': 'title'}).text
+                    review_data.append(recommended)
+                    hours_played = review.find('div', {'class': 'hours'}).text
+                    hours_played = hours_played.replace('\r', '')
+                    hours_played = hours_played.replace('\n', '')
+                    hours_played = hours_played.replace('\t', '')
+                    review_data.append(hours_played)
+                    writer.writerow(review_data)                        
+        else:
+            with open('output_files/resolved_steam_ids.csv', 'a') as F_2:
+                writer_2 = csv.writer(F_2)
+                writer_2.writerow([steam_id])
+                break
                 
 
